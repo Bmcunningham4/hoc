f2771db07211e590ed9d54c2afeb78d36d88d0d6
Ben Cunningham <bmcunningham4@outlook.com>
2025-10-15 21:47:03 +0800

    overflow and version for board

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,12 +4,12 @@ import trackImg from "./assets/track.jpg";
 export default function App() {
   return (
     <div
-      className="h-screen w-full flex flex-col items-center justify-center m-0 p-0 bg-cover bg-center bg-no-repeat"
+      className="h-screen w-full flex flex-col items-center justify-center m-0 p-0 bg-cover bg-center bg-no-repeat overflow-x-hidden"
       style={{
         backgroundImage: `url(${trackImg})`,
       }}
     >
-      <div className="w-[80%] flex justify-center">
+      <div className="w-[120%] flex justify-center">
         <Board />
       </div>
     </div>
