30094e417139149de85f9ba62da17773a7a53aef
Ben Cunningham <bmcunningham4@outlook.com>
2025-11-23 21:43:04 +0800

    delete other node_modules yuck

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,23 +1,7 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
-
-function HomePage() {
+export default function App() {
   return (
-    <div className="h-screen w-full flex items-center justify-center bg-blue-900">
-      <h1 className="text-white text-4xl">HashRouter Homepage Works!</h1>
+    <div className="h-screen bg-red-500 flex items-center justify-center">
+      <h1 className="text-white text-6xl">Suppppp boyyyy</h1>
     </div>
   );
 }
-
-export default function App() {
-  return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </HashRouter>
-  );
-}
-
-
-
-// HashRouter is my cba app setup, this is a piss take...
