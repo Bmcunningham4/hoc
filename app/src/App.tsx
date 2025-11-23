991cc570a436d146ca098a1d5854f7cbda5ecaca
Ben Cunningham <bmcunningham4@outlook.com>
2025-11-23 21:31:01 +0800

    fix git shite..

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function HomePage() {
   return (
     <div className="h-screen w-full flex items-center justify-center bg-blue-900">
-      <h1 className="text-white text-4xl">Homepage Works!</h1>
+      <h1 className="text-white text-4xl">HashRouter Homepage Works!</h1>
     </div>
   );
 }
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
+
+
+
+// HashRouter is my cba app setup, this is a piss take...
