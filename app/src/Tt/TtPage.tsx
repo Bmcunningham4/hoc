4df2cc7fa33f80e47746d3ab38c08ef624612411
Ben Cunningham <bmcunningham4@outlook.com>
2025-11-24 23:10:50 +0800

    Almost there..

--- app/src/Tt/TtPage.tsx
+++ app/src/Tt/TtPage.tsx
@@ -9,7 +9,18 @@ export default function TtPage() {
         backgroundImage: `url(${ttBgImg})`,
       }}
     >
-      <div className="w-full h-full flex justify-center">
+      {/* Header */}
+      <div className="flex flex-col items-center mb-4 mt-16">
+        <h1 className="text-black text-4xl font-bold drop-shadow-lg">
+          TT Championships
+        </h1>
+        <p className="text-black text-lg drop-shadow-md mt-1">
+          Cunningham v Harding
+        </p>
+      </div>
+
+      {/* Table */}
+      <div className="w-full flex-1 flex justify-center">
         <Table />
       </div>
     </div>
